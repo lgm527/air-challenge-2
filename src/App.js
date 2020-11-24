bbcabae203ff284496ffd922ce48eacfa64c29c4
lgm527 <laurellmccaffrey@gmail.com>
2020-11-24 16:31:03 -0500

    data available again

--- src/App.js
+++ src/App.js
@@ -2,31 +2,22 @@ import React from 'react';
 import Filter from './components/Filter';
 import Person from './components/Person';
 import './App.css';
+import data from './people.json';
+
+const ALLPEOPLE = data;
 
 class App extends React.Component {
 
   state = {
     loading: false,
-    people: [],
+    people: ALLPEOPLE,
     filter: '',
-    allPeople: [],
-  }
-
-  getPeople = () => {
-    fetch('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/090639df-57c7-4fd7-9472-67e00f8aa05f/MOCK_DATA_%282%29.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20201123%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201123T140032Z&X-Amz-Expires=86400&X-Amz-Signature=ea55262d76c25b7a8f2a85ff90c36899a083dc0af052f7cf54aa4b865c7b141e&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22MOCK_DATA%2520%282%29.json%22')
-    .then(res => res.json())
-    .then(people => this.setState({people: people, allPeople: people}))
-    .catch((error) => console.log(error))
   }
 
   handleChange = (event) => {
     let filter = event.currentTarget.value.toLowerCase()
     let filteredPeople = this.state.allPeople.filter((person) => person.name.toLowerCase().includes(filter))
     this.setState({people: filteredPeople, filter: event.currentTarget.value})
-  }
-
-  componentDidMount() {
-    this.getPeople();
   }
 
   render() {
